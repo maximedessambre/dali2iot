aa63914e8e4c74cd4ee28b281ce6f2447406fb29
Maxime <contact@maximedessambre.be>
2022-11-17 13:16:37 +0100

    add Dali2IoT is_on property

--- dali2iot.py
+++ dali2iot.py
@@ -81,8 +81,15 @@ class DaliLight(DaliDevice):
     """
     Dali Light device
     """
-    def __init__(self, id: int, name: str, info: str):
-        super().__init__(id, name, info, type=DALI_DEVICE_LIGHT)
+
+    def __init__(self, id: int, name: str, info: str, type: str, features=None,
+                 scenes=None, groups=None):
+        super().__init__(id=id, name=name, info=info, type=DALI_DEVICE_LIGHT, features=features, scenes=scenes,
+                         groups=groups)
+
+    @property
+    def is_on(self) -> bool:
+        return self.features['switchable']['status']
 
 
 class DALI2IoT:
